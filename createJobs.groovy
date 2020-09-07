7065836cdfe23bbdf05ed2dbfb62b6fd9f69a586
mbor <michal123borciuch@gmail.com>
2020-09-07 17:11:21 +0200

    Added new Job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/misiek001/SpringBootJenkins.git'
+                    }
+                    branch 'mymaster'
+                }
+            }
+        }
+    }
+}
